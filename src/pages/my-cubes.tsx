ad7b6cab9b57cb6778cd14aea73af43ae36b11e5
softgenai <admin@softgen.ai>
2025-08-12 22:42:46 +0000

    Edit src/pages/my-cubes.tsx

--- src/pages/my-cubes.tsx
+++ src/pages/my-cubes.tsx
@@ -420,7 +420,7 @@ export default function MyCubesPage() {
                   Create New TicketCube
                 </Button>
                 </div>
-             </div>
+             
             </>
           )}
         </div>
