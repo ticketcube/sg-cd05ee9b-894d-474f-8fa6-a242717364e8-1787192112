a8aa280814459a094d34730929086337e3ce59fb
softgenai <admin@softgen.ai>
2025-08-12 17:52:14 +0000

    Edit src/pages/my-cubes.tsx

--- src/pages/my-cubes.tsx
+++ src/pages/my-cubes.tsx
@@ -140,7 +140,7 @@ const CubePreview = ({ cube }: { cube: TicketCube }) => {
   }
 
   return (
-      <div className="w-full h-full">
+      <div className="w-full h-full aspect-square">
       <Canvas camera={{ position: [0, 0, 2], fov: 45 }}>
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
