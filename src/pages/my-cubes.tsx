6497dc05ce23d3f90c64ad8271a4fe9d3855440e
softgenai <admin@softgen.ai>
2025-08-13 16:48:03 +0000

    Edit src/pages/my-cubes.tsx

--- src/pages/my-cubes.tsx
+++ src/pages/my-cubes.tsx
@@ -343,7 +343,7 @@ export default function MyCubesPage() {
                                                 </div>
                                             </CardHeader>
                                         </Card>
-                                    ))}
+                                    )}
                                 </div>
 
 
