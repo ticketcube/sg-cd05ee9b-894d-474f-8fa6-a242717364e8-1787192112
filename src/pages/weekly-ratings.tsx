eef057c8396207b507e241a5a4685267d4ecf2b6
softgenai <admin@softgen.ai>
2025-08-13 21:26:31 +0000

    Edit src/pages/weekly-ratings.tsx

--- src/pages/weekly-ratings.tsx
+++ src/pages/weekly-ratings.tsx
@@ -269,7 +269,8 @@ function WeeklyRatingsPageContent() {
         <div className="max-w-md mx-auto">
           <Card className="bg-gray-900 border-gray-700">
             <CardContent className="p-4">
-              <div className="relative w-full h-80 bg-gray-800 rounded-lg border-2 border-gray-600">
+              <div className="relative w-72 h-64 bg-gray-800 rounded-lg border-2 border-gray-600">
+
                 <div className="absolute top-4 left-1/2 -translate-x-1/2 text-xs text-gray-400">Would Tell Friends</div>
                 <div className="absolute bottom-4 left-1/2 -translate-x-1/2 text-xs text-gray-400">Not For Them</div>
                 <div className="absolute left-4 top-1/2 -translate-y-1/2 -rotate-90 text-xs text-gray-400">Not For Me</div>
