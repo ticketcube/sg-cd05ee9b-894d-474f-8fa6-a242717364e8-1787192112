213e221f13356e66e4ce41ca70277c74785b8e6f
softgenai <admin@softgen.ai>
2025-08-13 22:05:05 +0000

    Revert to state at a46faa341730dfced0926e167717a40d219fec07

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 import AuthProvider from "@/contexts/AuthContext";
 import { CubeProvider } from "@/contexts/CubeContext";
 import { Toaster } from "@/components/ui/toaster";
-import { Layout } from '@/components/layout/Navbar'
+import { Layout } from '@/components/layout/Layout'
 
 
 export default function App({ Component, pageProps }: AppProps) {
