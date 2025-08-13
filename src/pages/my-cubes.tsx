87127500113ba513d50a3afbfcfe04c5888391fe
softgenai <admin@softgen.ai>
2025-08-13 15:10:56 +0000

    Edit src/pages/my-cubes.tsx

--- src/pages/my-cubes.tsx
+++ src/pages/my-cubes.tsx
@@ -380,7 +380,7 @@ export default function MyCubesPage() {
 
         {/* Right Column - Cube Preview */}
 
-                                                    <div className="flex flex-col justify-between space-y-4">
+                                                    <div className="space-y-4">
                                                         
                                                         <CubePreview cube={cube} />
 
