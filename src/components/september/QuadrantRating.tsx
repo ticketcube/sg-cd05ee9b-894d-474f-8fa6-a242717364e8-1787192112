cb93d8acf04a6946806597f9b82d3059e7cc963d
softgenai <admin@softgen.ai>
2025-09-12 22:44:00 +0000

    Edit src/components/september/QuadrantRating.tsx

--- src/components/september/QuadrantRating.tsx
+++ src/components/september/QuadrantRating.tsx
@@ -147,7 +147,7 @@ export function QuadrantRating({
             </div>
 
             {/* Rating Controls */}
-            <div className="flex-1 p-4 lg:p-6 min-h-0">
+            <div className="flex-1 min-h-0 flex flex-col justify-between">
                 <AnimatePresence>
                     <motion.div
                         initial={{ opacity: 0, y: 20 }}
