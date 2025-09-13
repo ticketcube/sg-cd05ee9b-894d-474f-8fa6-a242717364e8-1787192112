e7c4f40b9895167f38efbe2e3e0d21e41f327592
softgenai <admin@softgen.ai>
2025-09-13 17:59:57 +0000

    Edit tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,7 +27,9 @@ const config: Config = {
   			secondary: {
   				DEFAULT: 'hsl(var(--secondary))',
   				foreground: 'hsl(var(--secondary-foreground))'
-  			},
+                },
+            purpleDeep: { 'hsl(279, 92%, 25%)' },
+          },
   			muted: {
   				DEFAULT: 'hsl(var(--muted))',
   				foreground: 'hsl(var(--muted-foreground))'
