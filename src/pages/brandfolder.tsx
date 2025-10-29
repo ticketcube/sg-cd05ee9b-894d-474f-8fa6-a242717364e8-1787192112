ee0fa1c261d9dda9cfba66b4f4b4c0ab813457e3
softgenai <admin@softgen.ai>
2025-10-29 04:04:05 +0000

    Edit src/pages/brandfolder.tsx

--- src/pages/brandfolder.tsx
+++ src/pages/brandfolder.tsx
@@ -3,7 +3,7 @@ import { withAdminGuard } from "@/components/guards/withAdminGuard";
 import { brandfolderUploadPage } from "@/components/brandfolderUploadPage";
 
 function brandfolderUpload() {
-  return <brandfolderUpload />;
+  return <brandfolderUploadPage />;
 }
 
 export default withAdminGuard(brandfolderUpload);
