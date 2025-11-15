f78071f76e962cd62cecb5037906a05a7ea23df2
softgenai <admin@softgen.ai>
2025-11-15 08:06:54 +0000

    Edit src/components/NewsletterSignupOverlay.tsx

--- src/components/NewsletterSignupOverlay.tsx
+++ src/components/NewsletterSignupOverlay.tsx
@@ -111,14 +111,12 @@ export function NewsletterSignupOverlay({ onSubscribed, onClose }: NewsletterSig
               <strong className="text-gray-900">Welcome to OTW LIVE!</strong>
             </p>
             <p>
-                          As a member, once a week you'll receive an exclusive list of OTW Artists' shows in your city. 
-                      </p>
-                      <p>
-                          Discover Shows & Buy Tickets. v </p>
+                          As a member, once a week you'll receive an exclusive list of OTW Artists' shows in your city. Discover Shows & Buy Tickets.
+            </p>
             <p>
                     We Reward Discoverry      ™
 
-           
+            </p>
           </div>
           
 
